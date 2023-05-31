bc735c6299ffa711764cca58a9ed01c4c52a66a0
Sina Gh <sina.ghassai@gmail.com>
2023-05-31 23:15:18 +0330

    Extracted get started buttong to ui component

---
+++ src/components/UI/GetStartedButton.jsx
@@ -0,0 +1,27 @@
+import { Box, Button } from "@mui/material";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+const GetStartedButton = () => {
+  const auth = useSelector((state) => state.authenticator);
+  const nav = useNavigate();
+
+  const getStartedClickHandler = () => {
+    auth.isLoggedIn ? nav("/contacts") : nav("/login");
+  };
+
+  return (
+    <Box>
+      <Button
+        variant="contained"
+        color="primary"
+        className="animate-pulse"
+        onClick={getStartedClickHandler}
+      >
+        Get Started
+      </Button>
+    </Box>
+  );
+};
+
+export default GetStartedButton;
