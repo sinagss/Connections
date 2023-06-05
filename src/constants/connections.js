47b03ebf8423c2444b4ac15e96e62bc2e3ab86de
Sina Gh <sina.ghassai@gmail.com>
2023-06-05 10:20:53 +0330

    Created contant of predefined connections

---
+++ src/constants/connections.js
@@ -0,0 +1,484 @@
+export const connections = [
+  {
+    id: 1,
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+    birthDay: "1992-05-15",
+    sex: "Male",
+    city: "New York",
+    state: "New York",
+    address: "123 Main Street",
+    phoneNumbers: [
+      {
+        type: "Home",
+        number: "123-456-7890",
+      },
+      {
+        type: "Mobile",
+        number: "987-654-3210",
+      },
+    ],
+    emails: [
+      {
+        type: "Personal",
+        email: "johndoe@example.com",
+      },
+      {
+        type: "Work",
+        email: "john.doe@company.com",
+      },
+    ],
+    jobTitle: "Software Engineer",
+  },
+  {
+    id: 2,
+    firstName: "Jane",
+    lastName: "Smith",
+    age: 28,
+    birthDay: "1994-09-20",
+    sex: "Female",
+    city: "Los Angeles",
+    state: "California",
+    address: "456 Elm Street",
+    phoneNumbers: [
+      {
+        type: "Home",
+        number: "111-222-3333",
+      },
+      {
+        type: "Mobile",
+        number: "444-555-6666",
+      },
+    ],
+    emails: [
+      {
+        type: "Personal",
+        email: "jane.smith@example.com",
+      },
+      {
+        type: "Work",
+        email: "jane.smith@company.com",
+      },
+    ],
+    jobTitle: "Marketing Manager",
+  },
+  {
+    id: 3,
+    firstName: "David",
+    lastName: "Johnson",
+    age: 35,
+    birthDay: "1987-12-10",
+    sex: "Male",
+    city: "Chicago",
+    state: "Illinois",
+    address: "789 Oak Avenue",
+    phoneNumbers: [
+      {
+        type: "Home",
+        number: "555-123-4567",
+      },
+      {
+        type: "Mobile",
+        number: "111-222-3333",
+      },
+    ],
+    emails: [
+      {
+        type: "Personal",
+        email: "david.johnson@example.com",
+      },
+      {
+        type: "Work",
+        email: "david.johnson@company.com",
+      },
+    ],
+    jobTitle: "Accountant",
+  },
+  {
+    id: 4,
+    firstName: "Emily",
+    lastName: "Wilson",
+    age: 32,
+    birthDay: "1989-06-25",
+    sex: "Female",
+    city: "Seattle",
+    state: "Washington",
+    address: "567 Pine Street",
+    phoneNumbers: [
+      {
+        type: "Home",
+        number: "999-888-7777",
+      },
+      {
+        type: "Mobile",
+        number: "444-555-6666",
+      },
+    ],
+    emails: [
+      {
+        type: "Personal",
+        email: "emily.wilson@example.com",
+      },
+      {
+        type: "Work",
+        email: "emily.wilson@company.com",
+      },
+    ],
+    jobTitle: "Graphic Designer",
+  },
+  {
+    id: 5,
+    firstName: "Michael",
+    lastName: "Brown",
+    age: 42,
+    birthDay: "1979-03-12",
+    sex: "Male",
+    city: "Houston",
+    state: "Texas",
+    address: "321 Oak Street",
+    phoneNumbers: [
+      {
+        type: "Home",
+        number: "777-888-9999",
+      },
+      {
+        type: "Mobile",
+        number: "555-444-3333",
+      },
+    ],
+    emails: [
+      {
+        type: "Personal",
+        email: "michael.brown@example.com",
+      },
+      {
+        type: "Work",
+        email: "michael.brown@company.com",
+      },
+    ],
+    jobTitle: "Sales Manager",
+  },
+  {
+    id: 6,
+    firstName: "Sarah",
+    lastName: "Johnson",
+    age: 29,
+    birthDay: "1994-08-18",
+    sex: "Female",
+    city: "Boston",
+    state: "Massachusetts",
+    address: "789 Elm Street",
+    phoneNumbers: [
+      {
+        type: "Home",
+        number: "333-444-5555",
+      },
+      {
+        type: "Mobile",
+        number: "888-999-0000",
+      },
+    ],
+    emails: [
+      {
+        type: "Personal",
+        email: "sarah.johnson@example.com",
+      },
+      {
+        type: "Work",
+        email: "sarah.johnson@company.com",
+      },
+    ],
+    jobTitle: "Software Developer",
+  },
+  {
+    id: 7,
+    firstName: "Christopher",
+    lastName: "Taylor",
+    age: 34,
+    birthDay: "1988-09-26",
+    sex: "Male",
+    city: "San Francisco",
+    state: "California",
+    address: "123 Pine Street",
+    phoneNumbers: [
+      {
+        type: "Home",
+        number: "222-333-4444",
+      },
+      {
+        type: "Mobile",
+        number: "555-444-3333",
+      },
+    ],
+    emails: [
+      {
+        type: "Personal",
+        email: "christopher.taylor@example.com",
+      },
+      {
+        type: "Work",
+        email: "christopher.taylor@company.com",
+      },
+    ],
+    jobTitle: "Project Manager",
+  },
+  {
+    id: 8,
+    firstName: "Emma",
+    lastName: "Clark",
+    age: 27,
+    birthDay: "1995-06-02",
+    sex: "Female",
+    city: "Seattle",
+    state: "Washington",
+    address: "456 Oak Avenue",
+    phoneNumbers: [
+      {
+        type: "Home",
+        number: "777-888-9999",
+      },
+      {
+        type: "Mobile",
+        number: "111-222-3333",
+      },
+    ],
+    emails: [
+      {
+        type: "Personal",
+        email: "emma.clark@example.com",
+      },
+      {
+        type: "Work",
+        email: "emma.clark@company.com",
+      },
+    ],
+    jobTitle: "Marketing Coordinator",
+  },
+  {
+    id: 9,
+    firstName: "Jacob",
+    lastName: "Robinson",
+    age: 33,
+    birthDay: "1989-11-15",
+    sex: "Male",
+    city: "Denver",
+    state: "Colorado",
+    address: "789 Elm Street",
+    phoneNumbers: [
+      {
+        type: "Home",
+        number: "333-444-5555",
+      },
+      {
+        type: "Mobile",
+        number: "888-999-0000",
+      },
+    ],
+    emails: [
+      {
+        type: "Personal",
+        email: "jacob.robinson@example.com",
+      },
+      {
+        type: "Work",
+        email: "jacob.robinson@company.com",
+      },
+    ],
+    jobTitle: "Financial Analyst",
+  },
+  {
+    id: 10,
+    firstName: "Mia",
+    lastName: "Baker",
+    age: 31,
+    birthDay: "1991-12-10",
+    sex: "Female",
+    city: "Austin",
+    state: "Texas",
+    address: "654 Maple Lane",
+    phoneNumbers: [
+      {
+        type: "Home",
+        number: "222-333-4444",
+      },
+      {
+        type: "Mobile",
+        number: "555-444-3333",
+      },
+    ],
+    emails: [
+      {
+        type: "Personal",
+        email: "mia.baker@example.com",
+      },
+      {
+        type: "Work",
+        email: "mia.baker@company.com",
+      },
+    ],
+    jobTitle: "Product Manager",
+  },
+  {
+    id: 11,
+    firstName: "Liam",
+    lastName: "Gonzalez",
+    age: 29,
+    birthDay: "1993-05-27",
+    sex: "Male",
+    city: "Dallas",
+    state: "Texas",
+    address: "987 Elm Street",
+    phoneNumbers: [
+      {
+        type: "Home",
+        number: "333-444-5555",
+      },
+      {
+        type: "Mobile",
+        number: "888-999-0000",
+      },
+    ],
+    emails: [
+      {
+        type: "Personal",
+        email: "liam.gonzalez@example.com",
+      },
+      {
+        type: "Work",
+        email: "liam.gonzalez@company.com",
+      },
+    ],
+    jobTitle: "Software Engineer",
+  },
+  {
+    id: 12,
+    firstName: "Charlotte",
+    lastName: "Lopez",
+    age: 27,
+    birthDay: "1995-07-14",
+    sex: "Female",
+    city: "Phoenix",
+    state: "Arizona",
+    address: "321 Oak Street",
+    phoneNumbers: [
+      {
+        type: "Home",
+        number: "777-888-9999",
+      },
+      {
+        type: "Mobile",
+        number: "555-444-3333",
+      },
+    ],
+    emails: [
+      {
+        type: "Personal",
+        email: "charlotte.lopez@example.com",
+      },
+      {
+        type: "Work",
+        email: "charlotte.lopez@company.com",
+      },
+    ],
+    jobTitle: "Marketing Manager",
+  },
+  {
+    id: 13,
+    firstName: "Henry",
+    lastName: "Hernandez",
+    age: 35,
+    birthDay: "1987-03-02",
+    sex: "Male",
+    city: "Miami",
+    state: "Florida",
+    address: "543 Pine Street",
+    phoneNumbers: [
+      {
+        type: "Home",
+        number: "333-444-5555",
+      },
+      {
+        type: "Mobile",
+        number: "888-999-0000",
+      },
+    ],
+    emails: [
+      {
+        type: "Personal",
+        email: "henry.hernandez@example.com",
+      },
+      {
+        type: "Work",
+        email: "henry.hernandez@company.com",
+      },
+    ],
+    jobTitle: "Sales Manager",
+  },
+  {
+    id: 14,
+    firstName: "Amelia",
+    lastName: "Hall",
+    age: 30,
+    birthDay: "1992-10-20",
+    sex: "Female",
+    city: "Chicago",
+    state: "Illinois",
+    address: "456 Elm Street",
+    phoneNumbers: [
+      {
+        type: "Home",
+        number: "222-333-4444",
+      },
+      {
+        type: "Mobile",
+        number: "555-444-3333",
+      },
+    ],
+    emails: [
+      {
+        type: "Personal",
+        email: "amelia.hall@example.com",
+      },
+      {
+        type: "Work",
+        email: "amelia.hall@company.com",
+      },
+    ],
+    jobTitle: "HR Manager",
+  },
+  {
+    id: 15,
+    firstName: "William",
+    lastName: "Garcia",
+    age: 32,
+    birthDay: "1990-12-08",
+    sex: "Male",
+    city: "New York",
+    state: "New York",
+    address: "789 Oak Avenue",
+    phoneNumbers: [
+      {
+        type: "Home",
+        number: "777-888-9999",
+      },
+      {
+        type: "Mobile",
+        number: "111-222-3333",
+      },
+    ],
+    emails: [
+      {
+        type: "Personal",
+        email: "william.garcia@example.com",
+      },
+      {
+        type: "Work",
+        email: "william.garcia@company.com",
+      },
+    ],
+    jobTitle: "Finance Manager",
+  },
+];
+
+export default connections;
