7280b200997f0352588b3db36de4ecba2d3ab72c
Sina Gh <sina.ghassai@gmail.com>
2023-06-09 10:47:27 +0330

    Added theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "theme-blue": "#1976d2",
+        "theme-red": '#ef4444'
+
+      }
+    },
   },
   plugins: [],
 }
