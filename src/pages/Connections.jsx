37de36d4ae6af19e11712b6e368a54552efc6bd5
Sina Gh <sina.ghassai@gmail.com>
2023-06-10 21:09:12 +0330

    Fixed ui

--- src/pages/Connections.jsx
+++ src/pages/Connections.jsx
@@ -8,6 +8,7 @@ import {
   Fab,
   Toolbar,
   Container,
+  Box,
 } from "@mui/material";
 import { Add as AddIcon } from "@mui/icons-material";
 import NewConnection from "../components/NewConnection";
@@ -33,70 +34,70 @@ const Connections = () => {
   return (
     <>
       <Toolbar />
-      <Container className="mx-0 my-auto max-w-xl">
-        <Typography variant="h4" gutterBottom>
-          Connections
-        </Typography>
-        <List>
-          {contacts.map((contact, index) => (
-            <React.Fragment key={index}>
-              <ListItem
-                button
-                alignItems="flex-start"
-                sx={{
-                  cursor: "pointer",
-                  "&:hover": { backgroundColor: "action.hover" },
-                }}
-              >
-                <ListItemText
-                  primary={`${contact.firstName} ${contact.lastName}`}
-                />
-                <ListItemText
-                  primary="Phone:"
-                  secondary={
-                    <Typography
-                      component="span"
-                      variant="body2"
-                      color="textPrimary"
-                    >
-                      {contact.phoneNumbers[0]}
-                    </Typography>
-                  }
-                />
-                <ListItemText
-                  primary="Email:"
-                  secondary={
-                    <Typography
-                      component="span"
-                      variant="body2"
-                      color="textPrimary"
-                    >
-                      {contact.emails[0]}
-                    </Typography>
-                  }
-                />
-              </ListItem>
-              {index < contacts.length - 1 && (
-                <Divider variant="inset" component="li" />
-              )}
-            </React.Fragment>
-          ))}
-        </List>
-
-        <Fab
-          color="primary"
-          onClick={handleModalOpen}
-          sx={{ position: "fixed", bottom: 16, right: 16 }}
-        >
-          <AddIcon />
-        </Fab>
-
-        <NewConnection
-          open={isModalOpen}
-          onClose={handleModalClose}
-          onAddContact={handleAddContact}
-        />
-      </Container>
+      <Box>
+        <Container className="mx-0 my-auto max-w-xl">
+          <Typography variant="h4" gutterBottom>
+            Connections
+          </Typography>
+          <List>
+            {contacts.map((contact, index) => (
+              <React.Fragment key={index}>
+                <ListItem
+                  button
+                  alignItems="flex-start"
+                  sx={{
+                    cursor: "pointer",
+                    "&:hover": { backgroundColor: "action.hover" },
+                  }}
+                >
+                  <ListItemText
+                    primary={`${contact.firstName} ${contact.lastName}`}
+                  />
+                  <ListItemText
+                    primary="Phone:"
+                    secondary={
+                      <Typography
+                        component="span"
+                        variant="body2"
+                        color="textPrimary"
+                      >
+                        {contact.phoneNumbers[0]}
+                      </Typography>
+                    }
+                  />
+                  <ListItemText
+                    primary="Email:"
+                    secondary={
+                      <Typography
+                        component="span"
+                        variant="body2"
+                        color="textPrimary"
+                      >
+                        {contact.emails[0]}
+                      </Typography>
+                    }
+                  />
+                </ListItem>
+                {index < contacts.length - 1 && (
+                  <Divider variant="inset" component="li" />
+                )}
+              </React.Fragment>
+            ))}
+          </List>
+          <Fab
+            color="primary"
+            onClick={handleModalOpen}
+            sx={{ position: "fixed", bottom: 16, right: 16 }}
+          >
+            <AddIcon />
+          </Fab>
+          <NewConnection
+            open={isModalOpen}
+            onClose={handleModalClose}
+            onAddContact={handleAddContact}
+          />
+        </Container>
+      </Box>
     </>
   );
 };
