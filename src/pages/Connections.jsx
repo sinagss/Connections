a8ed2f7a8c6ed20367eadf5d067a270612aca6f3
Sina Gh <sina.ghassai@gmail.com>
2023-06-11 01:13:03 +0330

    Updated style and jsx

--- src/pages/Connections.jsx
+++ src/pages/Connections.jsx
@@ -1,20 +1,18 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import {
   Typography,
   List,
-  ListItem,
-  ListItemText,
-  Divider,
   Fab,
   Toolbar,
   Container,
   Box,
-  IconButton,
-  ListSubheader,
 } from "@mui/material";
-import { Add as AddIcon, Mail, Phone } from "@mui/icons-material";
+import {
+  Add as AddIcon,
+} from "@mui/icons-material";
 import NewConnection from "../components/NewConnection";
 import { useSelector } from "react-redux";
+import CustomListItem from "../components/UI/CustomListItem";
 
 const Connections = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -59,76 +57,7 @@ const Connections = () => {
             }}
           >
             {contacts.map((contact, index) => (
-              <React.Fragment key={index}>
-                <ListItem
-                  alignItems="flex-start"
-                  disablePadding
-                  disableGutters
-                  sx={{
-                    cursor: "pointer",
-                    display: "grid",
-                    gridTemplateColumns: {
-                      xs: "1fr 2fr 1fr 1fr",
-                      sm: "1fr 1fr 1fr 1fr",
-                    },
-                    "&:hover": { backgroundColor: "action.hover" },
-                  }}
-                  secondaryAction={
-                    <>
-                      <IconButton
-                        component="a"
-                        href={"mailto:" + contact.emails[0]}
-                      >
-                        <Mail />
-                      </IconButton>
-                      <IconButton
-                        component="a"
-                        href={`tel:${contact.phoneNumbers[0]}`}
-                      >
-                        <Phone />
-                      </IconButton>
-                    </>
-                  }
-                >
-                  <ListItemText
-                    sx={{ paddingRight: 14 }}
-                    primary={`${contact.firstName} ${contact.lastName}`}
-                  />
-                  <ListItemText
-                    primary="Phone:"
-                    secondary={
-                      <>
-                        <Typography
-                          sx={{ display: "flex", alignItems: "flex-end" }}
-                          component="span"
-                          variant="body2"
-                          color="textPrimary"
-                        ></Typography>
-                        {contact.phoneNumbers[0]}
-                      </>
-                    }
-                  />
-                  <ListItemText
-                    sx={{ display: { xs: "none", sm: "block" } }}
-                    primary="Email:"
-                    component="span"
-                    secondary={
-                      <>
-                        <Typography
-                          display="inline"
-                          component="span"
-                          variant="body2"
-                          color="textPrimary"
-                        ></Typography>
-                        {contact.emails[0]}
-                      </>
-                    }
-                  />
-                </ListItem>
-                {index < contacts.length - 1 && (
-                  <Divider variant="inset" component="li" />
-                )}
-              </React.Fragment>
+              <CustomListItem key={index} object={contact} index={index} />
             ))}
           </List>
           <Fab
