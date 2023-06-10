5f297ec1b63faaa1ba2988e4a20aaeb5cabd6c4e
Sina Gh <sina.ghassai@gmail.com>
2023-06-10 18:26:43 +0330

    Updated to store new connections as state

--- src/store/connectionsSlice.js
+++ src/store/connectionsSlice.js
@@ -1,14 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 import connections from "../constants/connections";
 
+const conCopy = [...connections];
+
 const connectionsSlice = createSlice({
   name: "connections",
-  initialState: connections,
+  initialState: {
+    connections: conCopy,
+  },
   reducers: {
-    addConnection: (state, action) => {},
+    addConnection: (state, action) => {
+      const randomId = Math.floor(
+        Math.random() * state.connections.length * 100
+      );
+      const newConnetion = { id: randomId, ...action.payload };
+
+      state.connections.push(newConnetion);
+      console.log(state.connections);
+    },
     removeConnection: (state, action) => {},
     editConnection: (state, action) => {},
   },
 });
+
+export const { addConnection, removeConnection, editConnection } =
+  connectionsSlice.actions;
 
 export default connectionsSlice.reducer;
