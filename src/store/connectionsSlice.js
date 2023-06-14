c0ec66139c7ee5bae4d64865cb454b3b0c6d708a
Sina Gh <sina.ghassai@gmail.com>
2023-06-14 11:18:18 +0330

    Updated and optimized Added remove connections reducer

--- src/store/connectionsSlice.js
+++ src/store/connectionsSlice.js
@@ -1,33 +1,51 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { connections } from "../constants/connections";
 
-const conCopy = [...connections];
-
 const connectionsSlice = createSlice({
   name: "connections",
   initialState: {
-    connections: conCopy,
+    connections: [...connections],
   },
   reducers: {
     addConnection: (state, action) => {
       const randomId = Math.floor(
         Math.random() * state.connections.length * 100
       );
-      const newConnetion = { id: randomId, ...action.payload };
+      const newConnection = { id: randomId, ...action.payload };
 
-      state.connections.push(newConnetion);
+      state.connections.push(newConnection);
     },
-    removeConnection: (state, action) => {},
-    editConnection: (state, action) => {},
-    favoriteConnection: (state, action) => {
-      const { object, fav } = action.payload;
-      const updatedConnections = state.connections.map((connection) =>
-        connection.id === object.id
-          ? { ...connection, favorite: fav }
-          : connection
+
+    removeConnection: (state, action) => {
+      const connectionId = action.payload;
+      state.connections = state.connections.filter(
+        (connection) => connection.id !== connectionId
+      );
+    },
+
+    updateConnection: (state, action) => {
+      const { id, contactInfo } = action.payload;
+      const connectionIndex = state.connections.findIndex(
+        (connection) => connection.id === id
       );
 
-      return { ...state, connections: updatedConnections };
+      if (connectionIndex !== -1) {
+        state.connections[connectionIndex] = {
+          ...state.connections[connectionIndex],
+          ...contactInfo,
+        };
+      }
+    },
+
+    favoriteConnection: (state, action) => {
+      const { object, fav } = action.payload;
+      const connectionIndex = state.connections.findIndex(
+        (connection) => connection.id === object.id
+      );
+
+      if (connectionIndex !== -1) {
+        state.connections[connectionIndex].favorite = fav;
+      }
     },
   },
 });
@@ -35,7 +53,7 @@ const connectionsSlice = createSlice({
 export const {
   addConnection,
   removeConnection,
-  editConnection,
+  updateConnection,
   favoriteConnection,
 } = connectionsSlice.actions;
 
