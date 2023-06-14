282f3fb76ec4742c660f274fc8b9fc0b7d915720
Sina Gh <sina.ghassai@gmail.com>
2023-06-14 11:16:50 +0330

    Update male and female valuse

---
+++ src/constants/strings.js
@@ -0,0 +1,36 @@
+const strings = {
+  en: {
+    home: {},
+    about: {},
+    connections: {
+      pageName: "Connections",
+      sort: {
+        label: "Sort",
+        fav: "⭐ First",
+        az: "A→Z",
+        za: "Z→A",
+      },
+      email: "Email",
+      phone: "Phone",
+    },
+    newConnections: {
+      firstName: "First Name",
+      lastName: "Last Name",
+      sex: { label: "Sex", female: "Female", male: "Male" },
+      age: "Age",
+      emails: {
+        emailsSectionLabel: "Emails",
+        emailLabel: "Email",
+        invalidEmail: "Invalid Email",
+      },
+      phoneNumbers: "Phone Numbers",
+      phone: "Phone",
+      address: "Address",
+      addNewConnection: "Add to Connections",
+      updateConnection: "Update Connection",
+    },
+  },
+  fa: {},
+};
+
+export default strings;
