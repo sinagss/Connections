9113348b4551324b09b22f66f1ae12d05e6cbaae
Sina Gh <sina.ghassai@gmail.com>
2023-06-15 10:43:41 +0330

    Added strings for Persian translation

--- src/constants/strings.js
+++ src/constants/strings.js
@@ -14,23 +14,71 @@ const strings = {
       phone: "Phone",
     },
     newConnections: {
-      firstName: "First Name",
-      lastName: "Last Name",
+      titleLabel: "Contact Information",
+      firstNameLabel: "First Name",
+      lastNameLabel: "Last Name",
       sex: { label: "Sex", female: "Female", male: "Male" },
-      age: "Age",
+      ageLabel: "Age",
+
       emails: {
         emailsSectionLabel: "Emails",
         emailLabel: "Email",
         invalidEmail: "Invalid Email",
+        addEmailLabel: "Add Email",
+        deleteEmailLabel: "Delete Email",
       },
-      phoneNumbers: "Phone Numbers",
-      phone: "Phone",
-      address: "Address",
+      phoneNumbers: {
+        phoneSectionLabel: "Phone Number",
+        phoneLabel: "Phone",
+        invalidPhone: "Invalid Phone Number",
+        addPhoneNumberLabel: "Add Phone Number",
+        deletePhoneNumberLabel: "Delete Phone Number",
+      },
+      addressLabel: "Address",
       addNewConnection: "Add to Connections",
       updateConnection: "Update Connection",
     },
   },
-  fa: {},
+  fr: {
+    home: {},
+    about: {},
+    connections: {
+      pageName: "روابط",
+      sort: {
+        label: "ترتیب",
+        fav: "⭐ اول",
+        az: "A→Z",
+        za: "Z→A",
+      },
+      email: "ایمیل",
+      phone: "شماره",
+    },
+    newConnections: {
+      titleLabel: "اطلاعات رابطه",
+      firstNameLabel: "نام",
+      lastNameLabel: "نام خانوادگی",
+      sex: { label: "Sex", female: "Female", male: "Male" },
+      ageLabel: "Age",
+
+      emails: {
+        emailsSectionLabel: "Emails",
+        emailLabel: "Email",
+        invalidEmail: "Invalid Email",
+        addEmailLabel: "Add Email",
+        deleteEmailLabel: "Delete Email",
+      },
+      phoneNumbers: {
+        phoneSectionLabel: "Phone Number",
+        phoneLabel: "Phone",
+        invalidPhone: "Invalid Phone Number",
+        addPhoneNumberLabel: "Add Phone Number",
+        deletePhoneNumberLabel: "Delete Phone Number",
+      },
+      addressLabel: "Address",
+      addNewConnection: "Add to Connections",
+      updateConnection: "Update Connection",
+    },
+  },
 };
 
 export default strings;
