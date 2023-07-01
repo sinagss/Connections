f912bbc5afca7b2b8e04c19c724b2c28590c3d6c
Sina Gh <sina.ghassai@gmail.com>
2023-07-01 10:17:57 +0330

    Updated the manifest and added icons

--- vite.config.js
+++ vite.config.js
@@ -10,17 +10,54 @@ export default defineConfig({
       manifest: {
         name: "Virtual Connections",
         short_name: "Connections",
+        theme_color: "#1976d2",
+        background_color: "#1976d2",
+        display: "standalone",
+        orientation: "portrait",
+        scope: "/",
         start_url: "/",
         icons: [
           {
-            src: "./src/assets/user.png",
-            sizes: "26x26",
+            src: "/src/assets/icons/icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "/src/assets/icons/icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "/src/assets/icons/icon-128x128.png",
+            sizes: "128x128",
+            type: "image/png",
+          },
+          {
+            src: "/src/assets/icons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "/src/assets/icons/icon-152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+          },
+          {
+            src: "/src/assets/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/src/assets/icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/src/assets/icons/icon-512x512.png",
+            sizes: "512x512",
             type: "image/png",
           },
         ],
-        background_color: "#3E4EB8",
-        display: "standalone",
-        theme_color: "#2E3AA1",
       },
       workbox: {
         // Customize the Workbox options here
